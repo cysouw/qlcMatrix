b93c7063925eae80ee72e9d3041af0225b21d6d2
Michael Cysouw <cysouw@mac.com>
2016-06-05 22:42:55 +0200

    adding svd decomposition

--- R/reduction.R
+++ R/reduction.R
@@ -1,16 +1,25 @@
-dimRed <- function(sim, k = 2) {
+dimRed <- function(sim, k = 2, method = "svd") {
   
-  ch <- Cholesky(sim)
-  M <- sparseMatrix( x = ch@x
-                   , p = ch@p
-                   , i = ch@i
-                   , index1 = FALSE
-  )
-  P <- as(ch, "pMatrix")
-  D <- diag(M)
-  diag(M) <- 1
-  L <- P %*% M[,1:k]
-  
-  return( list( L = L, D = D) )
-  
+  if (!is.na(pmatch(method,"cholesky"))) {
+    
+    ch <- Cholesky(sim)
+    M <- sparseMatrix( x = ch@x
+                     , p = ch@p
+                     , i = ch@i
+                     , index1 = FALSE
+    )
+    P <- as(ch, "pMatrix")
+    D <- diag(M)[1:k]
+    diag(M) <- 1
+    L <- P %*% M[,1:k]
+    
+    return( list( L = L, D = D) )
+    
+  } else if (!is.na(pmatch(method,"svd"))) {
+    
+    svd <- sparsesvd::sparsesvd(sim, rank = k)
+    return( list( L = svd$u, D = svd$d))
+    
+  }
+
 }
